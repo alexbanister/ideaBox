74f3d5d45beb192a43444b5f30d1a80e6637a731
Alex Banister <alexbanister@gmail.com>
2017-07-16 15:22:33 -0600

    beginnings of quality functions

--- javascript.js
+++ javascript.js
@@ -1,21 +1,37 @@
 //Array of objects for ideas
 //Expected Object structure
   // Obj = {
-  //   id:
-  //   title:
-  //   body:
-  //   quality:
+  //   id: getNewIdeaId(),
+  //   title: "string",
+  //   body: "string",
+  //   quality: num || 0
   // }
 var ideaBoxModel = [];
 retrieveModelFromLocalStorage();
-//TESTING STUFF
-var moreObj = {id: getNewIdeaId(), name: "bar", thing: "stuff" }
-var anotherObj = { id: getNewIdeaId(), name: "something", val: "foo" }
-var someObj = { id: getNewIdeaId(), name: "alex", place: "home" }
 
-insertIdeaToModel(moreObj);
-insertIdeaToModel(anotherObj);
-insertIdeaToModel(someObj);
+          //TESTING STUFF
+          var moreObj = {
+            id: getNewIdeaId(),
+            title: "foo",
+            body: "bar",
+            quality: 0
+          }
+          var anotherObj = {
+            id: getNewIdeaId(),
+            title: "things",
+            body: "stuff",
+            quality: 1
+          }
+          var someObj = {
+            id: getNewIdeaId(),
+            title: "blah",
+            body: "flub",
+            quality: 2
+          }
+          insertIdeaToModel(moreObj);
+          insertIdeaToModel(anotherObj);
+          insertIdeaToModel(someObj);
+          //TESTING STUFF
 
 function saveModelToLocalStorage() {
   idea = JSON.stringify(ideaBoxModel);
@@ -50,10 +66,14 @@ function getIdeaFromModel(id) {
   return idea;
 }
 
-function deleteIdeaFromModel(id) {
-  index = ideaBoxModel.findIndex(function(model) {
+function getIdeaIndex(id) {
+  return ideaBoxModel.findIndex(function(model) {
     return model.id === id;
   });
+}
+
+function deleteIdeaFromModel(id) {
+  var index = getIdeaIndex(id);
   if(index >= 0){
     ideaBoxModel.splice(index,1);
   }
@@ -70,4 +90,11 @@ function getNewIdeaId() {
   id++;
   saveIdeaIdCounterToLocalStorage(id);
   return id;
+}
+
+//Change idea quality functions
+function setQualityState(id) {
+  var 
+  index = getIdeaIndex(id);
+
 }
